a4a259ed3ba473182ae4e4ce5ddb4aa46477f767
DESKTOP-CFBG69B\IliyaDamyanov <i.s.damyanov@gmail.com>
2018-03-24 18:44:51 +0200

    HackerRank - Subdomain: Implementation: Repeated String task completed

---
+++ Algorithms/Implementation/Repeated String/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Repeated_String
+{
+    public class Program
+    {
+        static long repeatedString(string s, long n)
+        {
+            int numberOfAInString = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'a')
+                {
+                    numberOfAInString++;
+                }
+            }
+            long multiplier = n / s.Length;
+            long finalNumberOfA = numberOfAInString * multiplier;
+            long remainder = n % s.Length;
+            int numberOfAToAdd = 0;
+            for (int i = 0; i < remainder; i++)
+            {
+                if (s[i] == 'a')
+                {
+                    numberOfAToAdd++;
+                }
+            }
+            finalNumberOfA += numberOfAToAdd;
+            return finalNumberOfA;
+        }
+
+        static void Main(String[] args)
+        {
+            string s = Console.ReadLine();
+            long n = Convert.ToInt64(Console.ReadLine());
+            long result = repeatedString(s, n);
+            Console.WriteLine(result);
+        }
+    }
+}

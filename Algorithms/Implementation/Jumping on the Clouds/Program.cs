d09c4702389cebb4f1669ac4b7d9821732f1dcf2
DESKTOP-CFBG69B\IliyaDamyanov <i.s.damyanov@gmail.com>
2018-03-24 19:41:06 +0200

    HackerRank - Subdomain: Implementation:Jumping on the Clouds task completed

---
+++ Algorithms/Implementation/Jumping on the Clouds/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Jumping_on_the_Clouds
+{
+    public class Program
+    {
+        static int jumpingOnClouds(int[] c)
+        {
+            int counter = 0;
+            if (c.Length == 2)
+            {
+                return 1;
+            }
+            if (c.Length == 3)
+            {
+                return 2;
+            }
+            for (int i = 0; i < c.Length;)
+            {
+                if (c[i + 2] == 0)
+                {
+                    i += 2;
+                    counter++;
+                }
+                else if (c[i + 1] == 0)
+                {
+                    i++;
+                    counter++;
+                }
+                if (i == c.Length - 3 || i == c.Length - 2)
+                {
+                    counter++;
+                    break;
+                }
+            }
+            return counter;
+        }
+
+        static void Main()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] c_temp = Console.ReadLine().Split(' ');
+            int[] c = Array.ConvertAll(c_temp, Int32.Parse);
+            int result = jumpingOnClouds(c);
+            Console.WriteLine(result);
+        }
+    }
+}
